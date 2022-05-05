22a3ec746a91132442df0f14b85f6cc388818741
Daniel Volkonesko <eclipsemod555@gmail.com>
2022-05-05 19:10:03 +0300

    View component refactored

--- src/slider.view.ts
+++ src/slider.view.ts
@@ -2,18 +2,11 @@ import {Controller} from "./slider.controller";
 import {Model} from "./slider.model";
 import {SliderRangeOptions} from "./slider";
 
-class View {
-    public static render() {
-        Controller.getSlider();
-    }
-}
-
-class SliderRange extends View {
+class SliderRange {
     $el: JQuery<HTMLElement> = $(this.selector)
     constructor(private selector: string, private opts?: Partial<SliderRangeOptions>) {
-        super();
         this.$el.append(Model.slider)
-        SliderRange.render();
+        Controller.getSlider()
     }
 
 }

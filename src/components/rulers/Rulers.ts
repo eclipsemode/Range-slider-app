c323ffb0b3e205754f5fff1acb23ac309e7d1894
Daniel Volkonesko <eclipsemod555@gmail.com>
2022-08-09 11:06:45 +0300

    Hover to rulers values added

--- src/components/rulers/Rulers.ts
+++ src/components/rulers/Rulers.ts
@@ -11,12 +11,12 @@ class Rulers {
         return $(`
             <div class="slider-app__rulers js-slider-app__rulers"></div>
                 <div class="slider-app__rulers-values js-slider-app__rulers-values">
-                    <div class="slider-app__rulers-values--0">0</div>
-                    <div class="slider-app__rulers-values--20">20</div>
-                    <div class="slider-app__rulers-values--40">40</div>
-                    <div class="slider-app__rulers-values--60">60</div>
-                    <div class="slider-app__rulers-values--80">80</div>
-                    <div class="slider-app__rulers-values--100">100</div>
+                    <div class="slider-app__rulers-values--0"><span>0</span></div>
+                    <div class="slider-app__rulers-values--20"><span>20</span></div>
+                    <div class="slider-app__rulers-values--40"><span>40</span></div>
+                    <div class="slider-app__rulers-values--60"><span>60</span></div>
+                    <div class="slider-app__rulers-values--80"><span>80</span></div>
+                    <div class="slider-app__rulers-values--100"><span>100</span></div>
                 </div>
         `).prependTo(`${this.selector} .slider-app`);
     }
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,6 @@ import Controller from './Controller/slider.controller';
 
 new Controller('.slider', {
     configPanel: true,
-    percent: false,
-    min: 1000,
-    max: 28000,
-    rulers: false,
     tooltip: true,
     range: true,
 });

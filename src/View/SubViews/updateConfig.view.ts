9a550d5c7e421125947b84272f2d547c908aa07c
Daniel Volkonesko <eclipsemod555@gmail.com>
2022-09-24 18:43:58 +0300

    update Config and Control funcs refactored

--- src/View/SubViews/updateConfig.view.ts
+++ src/View/SubViews/updateConfig.view.ts
@@ -4,50 +4,57 @@ import { TogglesEnum} from '../../utils';
 function updateConfig ():void {
     const newSelector: string = this.selectorState.slice(1);
 
+    const handleConfig = (item: string, value: boolean): void => {
+        switch (item) {
+            case TogglesEnum.VERTICAL:
+                this.opts.vertical = value;
+                this.setBar();
+                this.setRulers();
+                this.setColor();
+                this.setControl();
+                this.setConfig();
+                this.setTooltip();
+                this.setVertical();
+                break;
+            case TogglesEnum.RULERS:
+                this.opts.rulers = value;
+                this.setRulers();
+                this.setColor();
+                this.setVertical();
+                break;
+            case TogglesEnum.PROGRESS:
+                this.opts.progress = value;
+                this.setBar();
+                this.setColor();
+                break;
+            case TogglesEnum.TOOLTIP:
+                this.opts.tooltip = value;
+                this.setTooltip();
+                this.setVertical();
+                break;
+            case TogglesEnum.RANGE:
+                this.opts.range = value;
+                this.setRange();
+                this.setBar();
+                this.setControl();
+                this.setConfig();
+                this.setTooltip();
+                this.setVertical();
+                this.thumbsObserver();
+                break;
+        }
+    };
+
     this.optionsState.toggleConfig.forEach((item: string): void => {
-        const element: JQuery = $(`#${ newSelector }__toggle-${ item }`);
+        const $element: JQuery = $(`#${ newSelector }__toggle-${ item }`);
 
-        element.on('change', (): void => {
-            const value: boolean = element.prop('checked');
-
-            switch (item) {
-                case TogglesEnum.VERTICAL:
-                    this.opts.vertical = value;
-                    this.setBar();
-                    this.setRulers();
-                    this.setColor();
-                    this.setControl();
-                    this.setConfig();
-                    this.setTooltip();
-                    this.setVertical();
-                    break;
-                case TogglesEnum.RULERS:
-                    this.opts.rulers = value;
-                    this.setRulers();
-                    this.setColor();
-                    this.setVertical();
-                    break;
-                case TogglesEnum.PROGRESS:
-                    this.opts.progress = value;
-                    this.setBar();
-                    this.setColor();
-                    break;
-                case TogglesEnum.TOOLTIP:
-                    this.opts.tooltip = value;
-                    this.setTooltip();
-                    this.setVertical();
-                    break;
-                case TogglesEnum.RANGE:
-                    this.opts.range = value;
-                    this.setRange();
-                    this.setBar();
-                    this.setControl();
-                    this.setConfig();
-                    this.setTooltip();
-                    this.setVertical();
-                    this.thumbsObserver();
-                    break;
-            }
+        $element.off('change', () => {
+            const value: boolean = $element.prop('checked');
+            handleConfig(item, value);
+        });
+        $element.on('change', () => {
+            const value: boolean = $element.prop('checked');
+            handleConfig(item, value);
         });
     });
 }
--- src/View/SubViews/updateControl.view.ts
+++ src/View/SubViews/updateControl.view.ts
@@ -5,37 +5,39 @@ import { ControlsEnum } from '../../utils';
 function updateControl(): void {
     const newSelector: string = this.selectorState.slice(1);
 
-    this.opts.controlConfig.forEach((item: string) => {
-        const element: JQuery = $(`#${ newSelector }__control-${ item }`);
+    const handleControl = (item: string): void => {
+        switch (item) {
+            case ControlsEnum.MIN:
+                this.setBar();
+                this.setTooltip();
+                this.setRulers();
+                this.setColor();
+                break;
+            case ControlsEnum.MAX:
+                this.setBar();
+                this.setTooltip();
+                this.setRulers();
+                this.setColor();
+                break;
+            case ControlsEnum.STEP:
+                this.setBar();
+                this.setTooltip();
+                break;
+            case ControlsEnum.FROM:
+                this.setBar();
+                this.setTooltip();
+                break;
+            case ControlsEnum.TO:
+                this.setBar();
+                this.setTooltip();
+                break;
+        }
+    };
 
-        element.on('input', () => {
-            switch (item) {
-                case ControlsEnum.MIN:
-                    this.setBar();
-                    this.setTooltip();
-                    this.setRulers();
-                    this.setColor();
-                    break;
-                case ControlsEnum.MAX:
-                    this.setBar();
-                    this.setTooltip();
-                    this.setRulers();
-                    this.setColor();
-                    break;
-                case ControlsEnum.STEP:
-                    this.setBar();
-                    this.setTooltip();
-                    break;
-                case ControlsEnum.FROM:
-                    this.setBar();
-                    this.setTooltip();
-                    break;
-                case ControlsEnum.TO:
-                    this.setBar();
-                    this.setTooltip();
-                    break;
-            }
-        });
+    this.opts.controlConfig.forEach((item: string) => {
+        const $element: JQuery = $(`#${ newSelector }__control-${ item }`);
+        $element.off('input', () => handleControl(item));
+        $element.on('input', () => handleControl(item));
     });
 }
 

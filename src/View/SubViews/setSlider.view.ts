912dfcb47b1409b78cf2fcd8463d30db304108e2
Daniel Volkonesko <eclipsemod555@gmail.com>
2022-10-07 18:11:32 +0300

    Revert "styles for vertical"
    This reverts commit a553b5a9923db29a304325df17bd250f3d7d4e91.

--- src/View/SubViews/setSlider.view.ts
+++ src/View/SubViews/setSlider.view.ts
@@ -5,9 +5,7 @@ import { MainClass } from '../../components';
 function setSlider(): void {
     const mainClass: MainClass = new MainClass(this.selectorState);
     mainClass.getMainClass();
-    const $sliderApp: JQuery = $(`${ this.selectorState } .slider-app`);
     $(this.selectorState).addClass('root');
-    $sliderApp.height($sliderApp.width());
 }
 
 export default setSlider;

ae55c1a925af9c4eda27a06148ece50a82050776
Daniel Volkonesko <eclipsemod555@gmail.com>
2022-10-24 23:35:10 +0300

    new slider without inputs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,32 @@
 import './assets/style/style.scss';
-import { Slider } from './Controller/slider.controller';
+import MouseMoveEvent = JQuery.MouseMoveEvent;
+import MouseDownEvent = JQuery.MouseDownEvent;
+// import { Slider } from './Controller/slider.controller';
 
-new Slider('.slider', {
-    configPanel: true,
-    tooltip: true,
-    range: false,
-    min: -1000
+// new Slider('.slider', {
+//     configPanel: true,
+//     tooltip: true,
+//     range: false,
+//     min: -1000
+// });
+
+const thumbMin: JQuery = $('.slider-app__thumb-min');
+
+thumbMin.on('mousedown', (e) => {
+
+    thumbMin.on('dragstart', () => false);
+    function moveAt(e: MouseMoveEvent | MouseDownEvent) {
+        thumbMin.css('left', e.pageX - thumbMin.width() + '%');
+    }
+
+    moveAt(e);
+
+    $(document).on('mousemove', (e) => {
+        moveAt(e);
+    });
+
+    thumbMin.on('mouseup', () => {
+        $(document).off('mousemove');
+        thumbMin.off('mouseup');
+    });
 });
